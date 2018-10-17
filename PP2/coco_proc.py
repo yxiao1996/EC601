cab5b24b30906e34933e0b44cfc11ceef1fcd97e
yxiao1996 <xiaoyu960522@sina.com>
2018-10-18 03:46:22 +0800

    Add keras-yolo model clone from reference repo

--- PP2/coco_proc.py
+++ PP2/coco_proc.py
@@ -68,7 +68,7 @@ class coco_worker(object):
             print(img_path)
             try:
                 I = cv2.imread(img_path)
-                boxes =  self._get_bound_box(img, catIds)
+                boxes, _ =  self._get_bound_box(img, catIds)
                 I = self._draw_bound_box(I, boxes)
                 cv2.imshow("Image Display", I)
                 cv2.waitKey(10)
@@ -111,13 +111,91 @@ class coco_worker(object):
 
         #print (anns)
         bound_box = []
+        class_box = []
         for instance in anns:
             bbox = np.array(instance['bbox']).astype(int)
-            print(bbox)
+            class_ = instance['category_id']
+            #print(instance)
             bound_box.append(bbox)
+            class_box.append(class_)
         
-        return  bound_box
+        return  bound_box, class_box
 
+    def gen_train_txt(self, classes, 
+                      save_dir = '.\\keras-yolo3\\',
+                      classes_save_dir = ".\\keras-yolo3\\model_data\\",
+                      data_size = 0):
+
+        # --------------------------------- #
+        # Create training data file for YOLO: train_classes.txt, train.txt
+        # Set argument data_size to 0 to generate all data
+        # --------------------------------- #
+
+        save_path = save_dir + "train.txt"
+        classes_save_path = classes_save_dir + "train_classes.txt"
+        save_file = open(save_path, "w")
+        classes_save_file = open(classes_save_path, "w")
+
+        # create a dictionary store category IDs
+        class_dict = {}
+
+        # Check all class in classes is legitimate
+        class_count = 0
+        for c in classes:
+            if (c not in self.cat_names):
+                print("class not in coco: " + c)
+                return
+            class_id = self.coco.getCatIds(c)
+            class_dict[class_id[0]] = class_count
+            class_count += 1
+            classes_save_file.write(c + '\n')
+
+        catIds = self.coco.getCatIds(catNms=classes)
+        imgIds = self.coco.getImgIds(catIds=catIds)
+
+        # Loop though the images
+        data_count = 0
+        print("data size: " + str(len(imgIds)))
+        for imgId in imgIds:
+            
+            if data_size != 0:
+                if (data_count >= data_size):
+                    break
+                else:
+                    data_count += 1
+
+            new_line = ""  # Init a new line
+
+            img = self.coco.loadImgs(imgId)[0]
+            img_path = self.dataDir + img['file_name']
+            new_line += img_path
+            
+            boxes, box_classes = self._get_bound_box(img, catIds)
+
+            for i, box in enumerate(boxes):
+
+                x_min = box[0]
+                y_min = box[1]
+                w = box[2]
+                h = box[3]
+                x_max = x_min + w
+                y_max = y_min + h
+
+                new_line += " " + str(x_min) + "," + str(y_min) + "," + str(x_max) + "," + str(y_max) + ","
+                
+                class_ = box_classes[i]
+                label = class_dict[class_]
+
+                new_line += str(label) + ' '
+
+
+            new_line += '\n'
+            save_file.write(new_line)
+
+        save_file.close()
+        classes_save_file.close()
+        print(data_count)
+        
 if __name__ == "__main__":
 
     sets = {'train': 'train2017',
@@ -126,8 +204,9 @@ if __name__ == "__main__":
     coco_path = 'D:\\downloads\\datasets\\'
 
     w = coco_worker(dataDir = coco_path,
-                    dataType = sets['val'])
+                    dataType = sets['train'])
 
-    classes = ['chair']
+    classes = ['tennis racket']
 
-    w.loop_classes(classes)
+    #w.loop_classes(classes)
+    w.gen_train_txt(classes)
